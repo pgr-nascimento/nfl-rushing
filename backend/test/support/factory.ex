4d962464b5fa18372fc04d9e96d033dc9e791a4e
Alan Nascimento <alanoliveiran45@gmail.com>
2021-08-23 22:18:09 -0300

    [API] Add player factory

--- backend/test/support/factory.ex
+++ backend/test/support/factory.ex
@@ -1,3 +1,23 @@
 defmodule NflRushing.Factory do
   use ExMachina.Ecto, repo: NflRushing.Repo
+
+  def player_factory do
+    %NflRushing.Player{
+      attempts: 2,
+      attempts_per_game_average: 2.0,
+      average_yards_per_attempt: 3.0,
+      first_downs: 1,
+      first_down_percentage: 2.2,
+      forty_plus_yards: 2,
+      longest_rush: 7,
+      name: "Joe Banyard",
+      position: "RB",
+      team_acronym: "JAX",
+      total_fumbles: 3,
+      total_touchdowns: 4,
+      total_yards: 3.0,
+      twenty_plus_yards: 5,
+      yards_per_game: 7.0
+    }
+  end
 end
