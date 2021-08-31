bce4275d57e1326f862d3393ff78d68adb6c8517
Alan Nascimento <alanoliveiran45@gmail.com>
2021-08-30 22:47:35 -0300

    Add PlayerView.get_path/4 fn to the project
    This fn will help the system to manage when the links should have a valid path or just return # to keep the user on the same page.

--- backend/lib/nfl_rushing_web/views/player_view.ex
+++ backend/lib/nfl_rushing_web/views/player_view.ex
@@ -40,4 +40,19 @@ defmodule NflRushingWeb.PlayerView do
       %{params | offset: total - limit}
     end
   end
+
+  def get_path(:previous_page, %{offset: 0}, _total, _conn), do: "#"
+
+  def get_path(:previous_page, params, _total, conn),
+    do: Routes.player_path(conn, :index, previous_page(params))
+
+  def get_path(:next_page, %{offset: offset, limit: limit} = params, total, conn) do
+    new_offset = offset + limit
+
+    if new_offset <= total do
+      Routes.player_path(conn, :index, next_page(params, total))
+    else
+      "#"
+    end
+  end
 end
