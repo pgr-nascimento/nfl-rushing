42431d3fbcdf83482671d4c68dbc7403bd527998
Alan Nascimento <alanoliveiran45@gmail.com>
2021-09-08 19:21:23 -0300

    Add Export.build_player/1 fn to the project

--- backend/lib/nfl_rushing/players/export.ex
+++ backend/lib/nfl_rushing/players/export.ex
@@ -1,4 +1,26 @@
 defmodule NflRushing.Players.Export do
+  alias NflRushing.Players.Player
+
+  def build_player(%Player{} = player) do
+    %{
+      "Player" => player.name,
+      "Team" => player.team_acronym,
+      "Pos" => player.position,
+      "Att/G" => player.attempts_per_game_average,
+      "Att" => player.attempts,
+      "Yds" => player.total_yards,
+      "Avg" => player.average_yards_per_attempt,
+      "Yds/G" => player.yards_per_game,
+      "TD" => player.total_touchdowns,
+      "Lng" => player.longest_rush,
+      "1st" => player.first_downs,
+      "1st%" => player.first_down_percentage,
+      "20+" => player.twenty_plus_yards,
+      "40+" => player.forty_plus_yards,
+      "FUM" => player.total_fumbles
+    }
+  end
+
   def build_headers() do
     [
       "Player",
--- backend/test/nfl_rushing/players/export_test.exs
+++ backend/test/nfl_rushing/players/export_test.exs
@@ -3,8 +3,6 @@ defmodule NflRushing.Players.ExportTest do
 
   alias NflRushing.Players.{Export, Player}
 
-  import NflRushing.Factory
-
   describe "build_headers/0" do
     test "it should returns the collumns summarized" do
       headers = [
@@ -27,5 +25,46 @@ defmodule NflRushing.Players.ExportTest do
 
       assert ^headers = Export.build_headers()
     end
+  end
+
+  describe "build_player/1" do
+    player = %Player{
+      attempts: 2,
+      attempts_per_game_average: 2.0,
+      average_yards_per_attempt: 3.0,
+      first_downs: 1,
+      first_down_percentage: 2.2,
+      forty_plus_yards: 2,
+      longest_rush: 7,
+      longest_rush_touchdown: false,
+      name: "Joe Banyard",
+      position: "RB",
+      team_acronym: "JAX",
+      total_fumbles: 3,
+      total_touchdowns: 4,
+      total_yards: 3.0,
+      twenty_plus_yards: 5,
+      yards_per_game: 7.0
+    }
+
+    converted_player = %{
+      "Player" => "Joe Banyard",
+      "Team" => "JAX",
+      "Pos" => "RB",
+      "Att/G" => 2.0,
+      "Att" => 2,
+      "Yds" => 3.0,
+      "Avg" => 3.0,
+      "Yds/G" => 7.0,
+      "TD" => 4,
+      "Lng" => 7,
+      "1st" => 1,
+      "1st%" => 2.2,
+      "20+" => 5,
+      "40+" => 2,
+      "FUM" => 3
+    }
+
+    assert ^converted_player = Export.build_player(player)
   end
 end
